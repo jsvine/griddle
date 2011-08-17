eb84fc136c2f54796f2ba5b2b0d6dfbac720d8fa
Jeremy Singer-Vine <jsvine@gmail.com>
2011-08-17 13:47:26 -0400

    Cleaned up and refactored, added multi-visible-rows and columns.
    === CHANGES ===
    Added missing .call
    
    Changed Griddle.init to Griddle.extend
    
    Removed px2int(), which was unused
    
    Changed grid class from 'griddle_grid' to 'griddle'
    
    Tweaked the valid-move checker in grid.move()
    
    Moved around some of the applyStyles calls
    
    Took assignments out of variable declarations, placed them afterward.
    
    Removed custom-arrow-button options, since constraints of grid should
      take care of that.
    
    Updated enableArrows settings to use .shift() instead of .move(),
    	per earlier update
    
    Added options to set number of visible rows & columns
    
    JSLint
    
    === TODO ===
    Throw errors if required vars missing

--- griddle.js
+++ griddle.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 	var root = this,
 		Griddle = {};
 	if (root.Griddle) {
@@ -6,67 +6,75 @@
 	} else {
 		root.Griddle = Griddle;	
 	}
+	// HELPER FUNCTIONS
+	function applyStyles(el, styles) {
+		var s;
+		for (s in styles) {
+			el.style[s] = styles[s];	
+		}
+		return el;
+	}
+	function CE(tag, className, innerHTML, styles) {
+		var el = document.createElement(tag);
+		if (className) { el.className = className; }
+		if (innerHTML) { el.innerHTML = innerHTML; }
+		if (styles) {
+			applyStyles(el, styles);
+		}
+		return el;
+	}
+	// `Tile` constructor	
+	function Tile(i) {
+		var x = i % this.n_col,
+			y = Math.floor(i / this.n_col);
+		return {
+			el: CE('div', 'griddle_tile'),
+			index: i,
+			x: x,
+			y: y,
+			adjacent: {
+				left: x > 0 ? i - 1 : false,
+				right: x < (this.n_col - 1) ? i + 1 : false,
+				up: y > 0 ? i - this.n_col : false,
+				down: y < ((this.tiles.length / this.n_col) - 1) ? i + this.n_col : false
+			}
+		};
+	}
+
 // `init` creates a new griddle, using `container` as the holder,
 // accepting any `attr.data`, a custom `render` function, and `options`.
-	Griddle.init = function (attr) {
-		var grid = {
-				container: attr.container,
-				el: CE('div', 'griddle_grid'),
-				n_col: attr.n_col || attr.data.length,
-				current_index: 0,
-				tiles: [],
-				goto: function (destination_index) {
-					var current_tile = this.tiles[this.current_index],
-						destination_tile = this.tiles[destination_index];
-					this.el.style.left = -(destination_tile.x * this.el.offsetWidth / this.n_col) + 'px';
-					this.el.style.top = -(destination_tile.y * this.el.offsetHeight * this.n_col / this.tiles.length) + 'px';
-					attr.onexit && attr.onexit.call(current_tile);
-					attr.onenter && attr.onenter.call(destination_tile);
-					this.current_index = destination_index;
-					return this;
-				},
-				shift: function (direction) {
-					var dest = this.tiles[this.current_index].adjacent[direction];
-					if (dest !== false) {
-						return this.goto(dest);
-					} else {
-						return false;
-					}
-				}
+	Griddle.extend = function (attr) {
+		var grid, tile, i, transition;
+		grid = {
+			container: attr.container,
+			el: CE('div', 'griddle'),
+			n_col: attr.n_col || attr.data.length,
+			n_col_visible: attr.n_col_visible || 1,
+			n_row_visible: attr.n_row_visible || 1,
+			current_index: 0,
+			tiles: [],
+			goto: function (destination_index) {
+				var current_tile = this.tiles[this.current_index],
+					destination_tile = this.tiles[destination_index];
+				this.el.style.left = -(destination_tile.x * this.el.offsetWidth / this.n_col) + 'px';
+				this.el.style.top = -(destination_tile.y * this.el.offsetHeight * this.n_col / this.tiles.length) + 'px';
+				if (attr.onexit) { attr.onexit.call(current_tile); }
+				if (attr.onenter) { attr.onenter.call(destination_tile); }
+				this.current_index = destination_index;
+				return this;
 			},
-			tile,
-			i,
-			transition = attr.transition || "left 0.25s linear, top 0.25s linear";
-		
-		applyStyles(grid.el, {
-			width: 100 * grid.n_col + '%',
-			height: 100 * attr.data.length / grid.n_col + '%',
-			position: 'relative',
-			overflow: 'auto',
-			top: 0,
-			left: 0,
-			WebkitTransition: transition,
-			MozTransition: transition,
-			OTransition: transition,
-			MsTransition: transition,	
-			Transition: transition
-		});
-		function Tile(i) {
-			var x = i % this.n_col,
-				y = Math.floor(i / this.n_col);
-			return {
-				el: CE('div', 'griddle_tile'),
-				index: i,
-				x: x,
-				y: y,
-				adjacent: {
-					left: x > 0 ? i - 1 : false,
-					right: x < (this.n_col - 1) ? i + 1 : false,
-					up: y > 0 ? i - this.n_col : false,
-					down: y < ((this.tiles.length / this.n_col) - 1) ? i + this.n_col : false
+			shift: function (direction) {
+				var dest = this.tiles[this.current_index].adjacent[direction];
+				if (this.tiles[dest]) {
+					return this.goto(dest);
+				} else {
+					return false;
 				}
-			};
-		}
+			}
+		};
+		
+		transition = attr.transition || "left 0.25s linear, top 0.25s linear";
+		
 		for (i = 0; i < attr.data.length; i++) {
 			tile = Tile.call(grid, i);
 			applyStyles(tile.el, {
@@ -81,72 +89,50 @@
 			grid.el.appendChild(tile.el);
 		}
 
-		attr.container.style.overflow = 'hidden';
-		attr.container.style.position = 'relative';
-		attr.container.appendChild(grid.el);
+		applyStyles(grid.el, {
+			width: 100 * grid.n_col / grid.n_col_visible + '%',
+			height: 100 * attr.data.length / (grid.n_col * grid.n_row_visible) + '%',
+			position: 'relative',
+			overflow: 'auto',
+			top: 0,
+			left: 0,
+			WebkitTransition: transition,
+			MozTransition: transition,
+			OTransition: transition,
+			MsTransition: transition,	
+			Transition: transition
+		});
 
+		applyStyles(attr.container, {
+			overflow: 'hidden',
+			position: 'relative'
+		});		
+		
 		if (attr.enableArrows) {
-			document.onkeydown = function(e){
-				var evt = e || window.event,
-					setting = attr.enableArrows;
-				
-				switch(evt.keyCode){
-					case 37: // Left
-						if (typeof setting === 'string' && setting.indexOf('l') === -1) {
-							break;
-						}
-						grid.move('left');
-						break;
-					case 38: // Up
-						if (typeof setting === 'string' && setting.indexOf('u') === -1) {
-							break;
-						}
-						grid.move('up');
-						break;
-					case 39: // Right
-						if (typeof setting === 'string' && setting.indexOf('r') === -1) {
-							break;
-						}
-						grid.move('right');
-						break;
-					case 40: // Down
-						if (typeof setting === 'string' && setting.indexOf('d') === -1) {
-							break;
-						}
-						grid.move('down');
-						break;
-					default:
-						break;
+			document.onkeydown = function (e) {
+				var evt = e || window.event;
+				switch (evt.keyCode) {
+				case 37: // Left
+					grid.shift('left');
+					break;
+				case 38: // Up
+					grid.shift('up');
+					break;
+				case 39: // Right
+					grid.shift('right');
+					break;
+				case 40: // Down
+					grid.shift('down');
+					break;
+				default:
+					break;
 				}	
 			};
 		}
+
+		attr.container.appendChild(grid.el);
 		return grid;
-	}
-
-	// HELPER FUNCTIONS
-	function CE(tag, cName, innerHTML, styles){
-		var el = document.createElement(tag);
-		if (cName) el.className = cName;
-		if (innerHTML) el.innerHTML = innerHTML;
-		if (styles) {
-			applyStyles(el, styles);
-		}
-		return el;
-	}
-	
-	function applyStyles(el, styles) {
-		var s;
-		for (s in styles) {
-			el.style[s] = styles[s];	
-		}
-		return el;
-	}
-
-	function px2int(str){
-		return parseInt(str.split('px')[0], 10) || 0;	
-	}
-
-
-})(this);
+	};
+}).call(this);
 
 
